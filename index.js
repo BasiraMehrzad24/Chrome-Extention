0f198eb9f5bccf0d17e227067fc45463a59f0e09
Basira Mehrzad <basiramehrzad25@gmail.com>
2024-07-11 08:02:33 +0430

    adding input value

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-   
+   inputEl.value= "";
     renderLeads()
 })
 
@@ -13,7 +13,7 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += "<li><a target='_blank' href=' " + myLeads[i] + " '> " + myLeads[i] + "</a></li>"
     }
     ulEl.innerHTML = listItems  
 }
